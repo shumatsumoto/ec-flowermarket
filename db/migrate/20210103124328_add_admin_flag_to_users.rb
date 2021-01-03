d5d2ac122c5558b9dbef247237af964a27bf9182
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-01-03 22:27:11 +0900

    User admin_flag 実装確認・修正

--- db/migrate/20210103124328_add_admin_flag_to_users.rb
+++ db/migrate/20210103124328_add_admin_flag_to_users.rb
@@ -1,5 +1,5 @@
 class AddAdminFlagToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :admin_flag, :boolean
+    add_column :users, :admin_flag, :boolean, default: false, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_01_03_124328) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.boolean "admin_flag"
+    t.boolean "admin_flag", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
