ca9876dd3891d95fcac4ac0cf34c85f6a7852201
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-01-05 15:28:33 +0900

    ダミーデータ挿入　seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do |i|
+	Product.create!(
+		name: "Flower #{i+1}",
+		price: "#{i*100}",
+		image: "/noimage.png",
+		summery: "Flower #{i+1}：商品の説明です。商品の説明です。商品の説明です。商品の説明です。商品の説明です。商品の説明です。商品の説明です。商品の説明です。"
+	)
+end
