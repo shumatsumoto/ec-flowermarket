2869ea14ae7ceb42d0decc051e4c78b13268c63f
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-01-06 21:56:24 +0900

    root url設定、 ログインしているかどうかの確認 （authenticate_user! ）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :require_admin, only: [:new, :edit, :create, :update, :destroy]
   before_action :search_product, only: [:index]
