14a653d044df9a5652c7f6b6e63961fd151b2c53
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-01-10 12:47:35 +0900

    jquery, bootstrap追加、カード登録・確認画面

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
   get 'pages/show'
   root to: 'products#index'
 
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
